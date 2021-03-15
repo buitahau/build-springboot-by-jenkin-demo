4f6e7c96b07c5dfaf6fafd12008f3df1fbbc85a1
ces-hau-bui <hau.bui@codeenginestudio.com>
2021-03-15 17:03:10 +0700

    Init project

---
+++ src/test/java/com/hau/bui/tutorial/themepartride/ThemeParkRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hau.bui.tutorial.themepartride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeParkRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
